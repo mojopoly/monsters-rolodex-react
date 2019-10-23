8e2945fc37de0a4b82bc670d33f260f5e1d579d4
Cyrus E <devcyruse@gmail.com>
2019-10-23 11:50:31 -0400

    Updates

---
+++ precache-manifest.6af8355e7865e8bf4036dfbc5d1934ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61909af76e7919ae58860cd30cb7fd30",
+    "url": "/mojopoly/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "3eab96999e776a3b7323",
+    "url": "/mojopoly/monsters-rolodex-react/static/css/main.c998d50f.chunk.css"
+  },
+  {
+    "revision": "60ef433d5db7c875689c",
+    "url": "/mojopoly/monsters-rolodex-react/static/js/2.6d4998bf.chunk.js"
+  },
+  {
+    "revision": "3eab96999e776a3b7323",
+    "url": "/mojopoly/monsters-rolodex-react/static/js/main.053a7ad8.chunk.js"
+  },
+  {
+    "revision": "34935c2b38d4d0ce1645",
+    "url": "/mojopoly/monsters-rolodex-react/static/js/runtime-main.5077cc3b.js"
+  }
+]);
